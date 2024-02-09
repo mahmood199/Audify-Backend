a52e70176cc9523c2ba825a72c7b67c57d108361
MSI <gaudentas199@gmail.com>
2024-02-09 20:19:58 +0530

    custom validation added

--- core/audify/serializers.py
+++ core/audify/serializers.py
@@ -17,3 +17,21 @@ class AudioSerializer(serializers.ModelSerializer):
             'language',
             'play_count'
         ]
+
+    def validate(self, data):
+        validation_errors = {}
+
+        if not data.get('title') or data.get('title').strip() == '':
+            validation_errors['title'] = "Title is mandatory"
+        if not data.get('release_year') and int(data.get('release_year')) == 2000:
+            validation_errors['release_year'] = "Release Year is mandatory"
+        if not data.get('artist') or data.get('artist').strip() == '':
+            validation_errors['artist'] = "Artist is mandatory"
+        if not data.get('url') or data.get('url').strip() == '':
+            validation_errors['url'] = "Url is mandatory"
+        if not data.get('download_url') or data.get('download_url').strip() == '':
+            validation_errors['download_url'] = "Download Url is mandatory"
+        if not data.get('language') or data.get('language').strip() == '':
+            validation_errors['language'] = "Language is mandatory"
+
+        return data
